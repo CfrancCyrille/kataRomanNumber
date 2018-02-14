636a7246bb9b2bbc58f8faaf0eb0f83e3dc094af
CfrancCyrille <francois.cyrille@gmail.com>
2018-02-14 16:47:58 +0100

    Init project

---
+++ src/main/java/kataRomanNumber/RomanNumber.java
@@ -0,0 +1,13 @@
+package kataRomanNumber;
+
+public class RomanNumber {
+
+	/**
+	 * Convert number from 1 to 3999 into roman number
+	 * @param nb up to 3999
+	 * @return roman number
+	 */
+	public String convert(int nb) {
+		return null;
+	}
+}
